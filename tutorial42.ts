c6d865851cb4c71b174dd16cc0fd2cbe7e8b7264
Vince <vince@dev.com>
2016-11-05 22:12:13 +0800

    added

---
+++ tutorial42.ts
@@ -0,0 +1,14 @@
+//type inference
+var a=10;
+a = 'hello'; //prevetion a should be int , not string
+
+var a2 = a + " String "; //type changed to String
+
+//any type 
+var info : any;
+info = 10;
+info = "hello";
+info = true;
+
+//enum for ts
+
